31c02389155e6f1672e67b6fa14af5d974fb283e
KJW <48905932+jiwonkim0131@users.noreply.github.com>
2024-05-29 20:28:08 +0900

    [jiwon] Container/Presentational Pattern

---
+++ week2/ContainerPresentational/jiwon/mockData.js
@@ -0,0 +1,6 @@
+export const mockData = [
+  { id: 1, name: 'Item 1', description: 'This is item 1' },
+  { id: 2, name: 'Item 2', description: 'This is item 2' },
+  { id: 3, name: 'Item 3', description: 'This is item 3' },
+  { id: 4, name: 'Item 4', description: 'This is item 4' },
+];
